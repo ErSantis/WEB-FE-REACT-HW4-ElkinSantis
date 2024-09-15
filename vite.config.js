fd8f176fb33014810da489d476620c6133cc6678
Elkin <ersantis@uninorte.edu.co>
2024-09-15 16:43:21 -0500

    Add base URL to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/WEB-FE-REACT-HW4-ElkinSantis/',  // Reemplaza <repository-name> por el nombre de tu repositorio en GitHub
 })
+
