fc7bbec3b022e5d8ace43e2925a45998d6dd5e88
Elkin <ersantis@uninorte.edu.co>
2024-09-15 17:26:20 -0500

    Refactor Objectives component to update heading size

--- src/components/Objectives.jsx
+++ src/components/Objectives.jsx
@@ -4,7 +4,7 @@ import ProductTable from './ProductTable';
 const Objectives = () => {
   return (
     <section className="p-6">
-      <h2 className="text-2xl font-semibold">3. Strategic Objectives</h2>
+      <h2 className="text-xl sm:text-2xl font-semibold">3. Strategic Objectives</h2>
       <ProductTable />
     </section>
   );
