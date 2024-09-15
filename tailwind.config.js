cf035e16d3ed363b45ef580233569e40192657d9
Elkin <ersantis@uninorte.edu.co>
2024-09-15 16:43:16 -0500

    Add new custom colors to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'digital-purple-dark': '#42326E',
+        'digital-purple-medium': '#6E5B9A',
+        'digital-purple-light': '#B29CE4',
+        'digital-lavender-soft': '#B2A6CE',
+        'digital-lavender-light': '#D7C8ED',
+        'digital-lavender-very-light': '#E0D4FC',
+      },
+    },
   },
   plugins: [],
 };
